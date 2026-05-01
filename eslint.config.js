67220f5646838df23c104eb6b5b639ec7908aeca
Leonardo Lazarini <leo.lazarini@gmail.com>
2026-05-01 02:30:58 -0300

    chore: disable no-explicit-any for test files

--- eslint.config.js
+++ eslint.config.js
@@ -11,5 +11,11 @@ export default tseslint.config(
       '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
       '@typescript-eslint/explicit-function-return-type': 'off',
     },
+  },
+  {
+    files: ['test/**/*.ts'],
+    rules: {
+      '@typescript-eslint/no-explicit-any': 'off',
+    },
   }
 )
